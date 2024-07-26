cc543639c1184eaee48a2eba62e0d6d115b17438
GenissonEmilio <esgenisson@gmail.com>
2024-07-26 20:26:08 -0300

    update

---
+++ html-css/Exercícios/ex026/mq005/main.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+  let menu = document.getElementById('men');
+  let burguer = document.getElementById('burguer');
+  
+  burguer.addEventListener('click', () => {
+    if (menu.style.display == 'block') {
+      menu.style.display = 'none';
+    } else {
+      menu.style.display = 'block';
+    }
+  });
+  
+})
